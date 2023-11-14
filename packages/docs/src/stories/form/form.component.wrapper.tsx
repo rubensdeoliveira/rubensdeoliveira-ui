3660c01bf3e9ca780be3b3578524880c7567b490
Rubens Junior <rubensjunior@Rubens-2.local>
2023-11-13 23:49:30 -0300

    fix: fix deploy

--- packages/docs/src/stories/form/form.component.wrapper.tsx
+++ packages/docs/src/stories/form/form.component.wrapper.tsx
@@ -1,19 +1,4 @@
-import {
-  Form,
-  InputText,
-  Button,
-  InputSelect,
-  InputAutoComplete,
-  InputCheckbox,
-  InputCurrency,
-  InputDate,
-  InputMaskedText,
-  FormRow,
-  InputMultSelect,
-  InputRadio,
-  InputSwitch,
-  InputTextArea,
-} from '@rubensdeoliveira-ui/react'
+import { Form, Button, InputMultSelect } from '@rubensdeoliveira-ui/react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -44,84 +29,16 @@ export function FormWrapper() {
   return (
     <div className="flex flex-col gap-10 bg-red-300 p-10 rounded">
       <Form onSubmit={handleSubmit(handleSubmitForm)} className="gap-4">
-        <FormRow>
-          <InputText name="name" control={control} />
-        </FormRow>
-        <FormRow>
-          <InputText name="email" control={control} containerClassName="px-0" />
-          <InputText name="password" control={control} type="password" />
-        </FormRow>
-        <FormRow>
-          <InputSelect
-            control={control}
-            name="occupation"
-            options={[
-              { label: 'Programador', value: 'programador' },
-              { label: 'Engenheiro', value: 'engenheiro' },
-              { label: 'Dentista', value: 'dentista' },
-            ]}
-          />
-        </FormRow>
-        <FormRow>
-          <InputAutoComplete
-            control={control}
-            name="sport"
-            options={[
-              { label: 'Futebol', value: 'futebol' },
-              { label: 'Vôlei', value: 'vôlei' },
-              { label: 'Atletismo', value: 'atletismo' },
-            ]}
-          />
-        </FormRow>
-        <FormRow>
-          <InputCheckbox
-            control={control}
-            name="acceptTerms"
-            label="Aceito os termos"
-          />
-        </FormRow>
-        <FormRow>
-          <InputCurrency control={control} name="salary" />
-        </FormRow>
-        <FormRow>
-          <InputDate control={control} name="birthday" />
-        </FormRow>
-        <FormRow>
-          <InputMaskedText control={control} name="phone" mask="telefone" />
-        </FormRow>
-        <FormRow>
-          <InputMultSelect
-            control={control}
-            name="hobbies"
-            options={[
-              { label: 'Karate', value: 'karate' },
-              { label: 'Futebol', value: 'futebol' },
-              { label: 'Cantar', value: 'cantar' },
-            ]}
-          />
-        </FormRow>
-        <FormRow>
-          <InputRadio
-            control={control}
-            name="gender"
-            options={[
-              { label: 'Masculino', value: 'male' },
-              { label: 'Feminino', value: 'female' },
-            ]}
-            renderRadioItemBody={(item, option) => (
-              <span className={item.checked ? 'text-red-500' : 'text-blue-500'}>
-                {option.label}
-              </span>
-            )}
-          />
-        </FormRow>
-        <FormRow>
-          <InputSwitch control={control} name="isOrganDonor" />
-        </FormRow>
-        <FormRow>
-          <InputTextArea rows={10} control={control} name="about" />
-        </FormRow>
-        <Button label="Enviar" className="bg-red-500" />
+        <InputMultSelect
+          control={control}
+          name="hobbies"
+          options={[
+            { label: 'Karate', value: 'karate' },
+            { label: 'Futebol', value: 'futebol' },
+            { label: 'Cantar', value: 'cantar' },
+          ]}
+        />
+        <Button label="Enviar" containerClassName="bg-red-500" />
       </Form>
 
       <div className="bg-white p-4 shadow-md rounded-lg">
